2e71f3e1a80a9316e727ba82ba9ec8693c987223
Dmitry <da-boyko@mail.ru>
2024-06-08 17:52:36 +0700

    ref: change module verbose_names

--- website/apps.py
+++ website/apps.py
@@ -1,7 +1,11 @@
 from django.apps import AppConfig
+from django.core.cache import cache
+from django.db.models.signals import post_delete, post_save
+from django.dispatch import receiver
 
 
 class WebsiteConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "website"
-    verbose_name = "интерфейс"
+    verbose_name = "Интерфейс"
+
