4fb1fbf85ca405a08d3b6c9b534a126e3712d5d1
Dmitry <da-boyko@mail.ru>
2024-06-08 16:48:47 +0700

    ref: move swagger urls to main url config

--- config/urls.py
+++ config/urls.py
@@ -12,6 +12,14 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+    urlpatterns += [
+        path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+        path("api/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),
+        path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    ]
+
     urlpatterns += static(
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT,
