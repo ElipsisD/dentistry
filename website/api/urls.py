ab5cd494b7a9e74d36b33c1a123775a2a44cacd1
Dmitry <da-boyko@mail.ru>
2024-06-22 12:36:48 +0700

    feat: add discount and service API

--- website/api/urls.py
+++ website/api/urls.py
@@ -2,8 +2,10 @@ from django.urls import include, path
 from rest_framework import routers
 
 from website.api.handlers.contacts import ContactsAPI
+from website.api.handlers.discount import DiscountAPI
 from website.api.handlers.notification import NotificationAPI
 from website.api.handlers.price import PriceAPI
+from website.api.handlers.service import ServiceAPI
 from website.api.handlers.specialist import CarouselSpecialistAPI, SpecialistAPI
 
 router = routers.DefaultRouter()
@@ -13,6 +15,8 @@ router.register("carousel-specialists", CarouselSpecialistAPI, basename="carouse
 router.register("contacts", ContactsAPI, basename="contacts")
 router.register("notification", NotificationAPI, basename="notification")
 router.register("price", PriceAPI, basename="price")
+router.register("discount", DiscountAPI, basename="discount")
+router.register("service", ServiceAPI, basename="service")
 
 urlpatterns = [
     path("", include(router.urls)),
