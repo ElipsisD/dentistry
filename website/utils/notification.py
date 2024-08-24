5f72b30a8cbfb8549f443a090cd54f3fd88cd6ec
Dmitry <da-boyko@mail.ru>
2024-08-24 15:43:57 +0700

    feat: create method for create notification about application

--- website/utils/notification.py
+++ website/utils/notification.py
@@ -6,6 +6,7 @@ from django.core.mail import send_mail
 from config.settings import BOT_TOKEN, EMAIL_HOST_USER
 from core.models import Config
 from website.models import Specialist
+from website.models.application import IssueType
 from website.models.notification import NotificationType
 
 CALLBACK_TEMPLATE = """Поступил запрос на обратный звонок!\n
@@ -23,6 +24,15 @@ FEEDBACK_TEMPLATE = """Поступил новый отзыв!\n
 <b>Телефон:</b> {}
 <b>Оценка:</b> {}
 <b>Отзыв:</b> {}"""
+
+APPLICATION_TEMPLATE = """Поступила заявка на подготовку справки!\n
+<b>Имя:</b> {}
+<b>Имя налогоплательщика:</b> {}
+<b>ИНН налогоплательщика:</b> {}
+<b>Отчетный год:</b> {}
+<b>Вариант получения:</b> {}
+<b>Email:</b> {}
+<b>Телефон:</b> {}"""
 
 
 def make_telegram_notification(message_text: str) -> None:
@@ -51,6 +61,19 @@ def get_feedback_message_text(data: OrderedDict) -> str:
         data["phone_number"],
         data["rating"],
         data["feedback"],
+    )
+
+
+def get_application_message_text(data: OrderedDict) -> str:
+    """Return text message for application."""
+    return APPLICATION_TEMPLATE.format(
+        data["name"],
+        data["taxpayer_name"],
+        data["INN"],
+        data["reporting_year"],
+        dict(IssueType.choices)[data["issue_type"]],
+        data["email"],
+        data["phone_number"],
     )
 
 
