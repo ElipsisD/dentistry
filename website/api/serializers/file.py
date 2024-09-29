947ec81552ac62b2275114c83c8c5e3c8491dfa0
Dmitry <da-boyko@mail.ru>
2024-09-29 12:23:27 +0700

    fix: change serializer model

--- website/api/serializers/file.py
+++ website/api/serializers/file.py
@@ -1,16 +1,16 @@
 from rest_framework import serializers
 
-from website.models import PriceFile
+from website.models import File
 
 
 class FileSerializer(serializers.ModelSerializer):
     file = serializers.SerializerMethodField()
 
     class Meta:
-        model = PriceFile
+        model = File
         fields = "__all__"
 
-    def get_file(self, instance: PriceFile) -> str | None:
+    def get_file(self, instance: File) -> str | None:
         request = self.context.get("request")
         if instance.file and request:
             return request.build_absolute_uri(instance.file.url)
